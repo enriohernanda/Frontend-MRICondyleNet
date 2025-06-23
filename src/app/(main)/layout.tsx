6c8a118cf1b010ffc35b55a55131be924c9b8254
enriohernanda <enriohernanda@gmail.com>
2025-06-23 13:21:30 +0700

    update layout main

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -17,17 +17,16 @@ const MainLayout = ({ children }: { children: ReactNode }) => {
   }, [router]);
 
   return (
-    <div className="min-h-screen w-full overflow-x-hidden bg-[#EBEBEB] dark:bg-[#0D1117]">
+    <div className="h-screen w-full flex flex-col overflow-hidden bg-[#EBEBEB] dark:bg-[#0D1117]">
       {/* Navbar */}
       <NavbarMain />
 
-      {/* Wrapper untuk sidebar & konten */}
-      <div className="flex">
-        {/* Sidebar */}
+      {/* Sidebar + Main Content */}
+      <div className="flex flex-1 overflow-hidden">
         <Sidebar />
-
-        {/* Main Content */}
-        <main className="flex-1 p-4 text-black dark:text-white">{children}</main>
+        <main className="flex-1 overflow-y-auto p-4 text-black dark:text-white">
+          {children}
+        </main>
       </div>
     </div>
   );
