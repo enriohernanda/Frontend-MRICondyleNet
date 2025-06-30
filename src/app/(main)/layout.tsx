315fc171e542fff176be115bb171ab43e4c916d4
enriohernanda <enriohernanda@gmail.com>
2025-06-30 23:00:11 +0700

    update layout main

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,24 +4,29 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import NavbarMain from '@/components/main/NavbarMain';
 import Sidebar from '@/components/main/Sidebar';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      router.push('/login');
+    // Hanya jalan di client
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        router.push('/login');
+      } else {
+        setMounted(true); // render layout setelah cek selesai
+      }
     }
   }, [router]);
 
+  if (!mounted) return null;
+
   return (
     <div className="h-screen w-full flex flex-col overflow-hidden bg-[#EBEBEB] dark:bg-[#0D1117]">
-      {/* Navbar */}
       <NavbarMain />
-
-      {/* Sidebar + Main Content */}
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <main className="flex-1 overflow-y-auto p-4 text-black dark:text-white">
