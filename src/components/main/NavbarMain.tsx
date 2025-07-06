7b8fc01a747a11b6939a0ea68fdd6139827b5a58
enriohernanda <enriohernanda@gmail.com>
2025-07-06 14:57:43 +0700

    update navbar main

--- src/components/main/NavbarMain.tsx
+++ src/components/main/NavbarMain.tsx
@@ -82,9 +82,7 @@ const Navbar = () => {
     <nav className="flex justify-between items-center px-6 py-4 bg-[#578FCA] dark:bg-[#161B22] shadow-md">
       <div className="flex items-center">
         <img src="/logo-MRICondyleNet.png" alt="Logo" className="h-8 md:h-10" />
-        <span className="hidden md:block md:text-md lg:text-xl font-bold ml-2 text-white">
-          MRICondyleNET
-        </span>
+        <span className="hidden md:block md:text-md lg:text-xl font-bold ml-2 text-white">MRICondyleNET</span>
       </div>
 
       <div className="flex items-center gap-6">
@@ -92,29 +90,18 @@ const Navbar = () => {
         <div className="relative">
           <button onClick={toggleDropdown} className="flex items-center text-lg gap-2 text-white">
             <div className="w-10 h-10 rounded-full overflow-hidden bg-[#D9D9D9] flex items-center justify-center">
-              <img
-                src={profileUrl}
-                alt="User Profile"
-                onError={(e) => (e.currentTarget.src = '/photos.png')}
-                className="w-full h-full object-cover"
-              />
+              <img src={profileUrl} alt="User Profile" onError={(e) => (e.currentTarget.src = '/photos.png')} className="w-full h-full" />
             </div>
             <FaChevronDown className={`cursor-pointer transition-transform ${isDropdownOpen ? 'rotate-180' : ''}`} />
           </button>
 
           {isDropdownOpen && (
             <div className="absolute right-0 mt-2 w-36 bg-[#578FCA] dark:bg-[#161B22] rounded-lg shadow-lg overflow-hidden z-50">
-              <button
-                onClick={() => router.push('/profile')}
-                className="block w-full text-left px-4 py-2 text-white hover:bg-[#5774ca] dark:hover:bg-[#30363D] cursor-pointer"
-              >
+              <button onClick={() => router.push('/profile')} className="block w-full text-left px-4 py-2 text-white hover:bg-[#5774ca] dark:hover:bg-[#30363D] cursor-pointer">
                 Profile
               </button>
               <hr className="border-gray-200 dark:border-gray-600" />
-              <button
-                onClick={handleLogout}
-                className="block w-full text-left px-4 py-2 text-white hover:bg-[#5774ca] dark:hover:bg-[#30363D] cursor-pointer"
-              >
+              <button onClick={handleLogout} className="block w-full text-left px-4 py-2 text-white hover:bg-[#5774ca] dark:hover:bg-[#30363D] cursor-pointer">
                 Log Out
               </button>
             </div>
