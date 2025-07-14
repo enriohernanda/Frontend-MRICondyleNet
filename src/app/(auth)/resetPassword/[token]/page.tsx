50e972c3e126d43b8779aeb61c0cb5b8fa92dfe3
enriohernanda <enriohernanda@gmail.com>
2025-07-14 14:27:22 +0700

    add reset password page

---
+++ src/app/(auth)/resetPassword/[token]/page.tsx
@@ -0,0 +1,17 @@
+import LeftBgLight from '@/components/LeftBgLight';
+import LogoBanner from '@/components/LogoBanner';
+import ResetPasswordPage from '@/components/resetPassPage/ResetPasswordPage';
+
+export default function Page({ params }: { params: { token: string } }) {
+  return (
+    <div className="flex justify-center flex-col md:flex-row min-h-screen w-full 
+                bg-[#3674B5] 
+                dark:bg-[#0D1117] lg:bg-none">
+      <div className="absolute top-0 w-full z-20">
+        <LogoBanner />
+      </div>
+      <LeftBgLight />
+      <ResetPasswordPage token={params.token} />
+    </div>
+  );
+}
