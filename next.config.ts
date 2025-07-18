5f1003ad5ead4cc235a89deb78509d32fe43523f
enriohernanda <enriohernanda@gmail.com>
2025-07-19 02:58:34 +0700

    update Next Config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  "rules": {
-  "@typescript-eslint/no-unused-vars": "off",
-  "@typescript-eslint/no-explicit-any": "off",
-  "react-hooks/exhaustive-deps": "off"
-}
 
 };
 
