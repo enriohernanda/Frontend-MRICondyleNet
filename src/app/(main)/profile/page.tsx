b185ebf7957f3b34384f2c67752231327235afa5
enriohernanda <enriohernanda@gmail.com>
2025-07-18 23:58:10 +0700

    revisi profile page

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -2,16 +2,31 @@
 
 import ProfileForm from '@/components/profileComponents/ProfileForm';
 import React from 'react';
+import Image from 'next/image';
 
 const ProfilePage = () => {
   return (
     <div className="w-full h-full px-4 py-6 text-black dark:text-white">
       <div className="mb-6">
-        <h1 className="text-3xl font-bold flex items-center gap-2">
-          <img src="/profile-blue.png" alt="Profile Icon Light" className="block dark:hidden w-8 h-8" />
-          <img src="/profile-white.png" alt="Profile Icon Dark" className="hidden dark:block w-8 h-8" />
-          Profile
-        </h1>
+        <h1 className="text-2xl sm:text-3xl font-bold flex items-center gap-2">
+                  {/* Icon Light Mode */}
+                  <Image
+                    src="/profile-blue.png"
+                    alt="Profile Icon Light"
+                    width={32}
+                    height={32}
+                    className="block dark:hidden"
+                  />
+                  {/* Icon Dark Mode */}
+                  <Image
+                    src="/profile-white.png"
+                    alt="Profile Icon Dark"
+                    width={32}
+                    height={32}
+                    className="hidden dark:block"
+                  />
+                  Profile
+                </h1>
       </div>
       <ProfileForm />
     </div>
