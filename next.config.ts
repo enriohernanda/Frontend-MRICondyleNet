20d18cf33142edc9de064f3c43320c6920348349
enriohernanda <enriohernanda@gmail.com>
2025-07-25 17:56:14 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['https://f8af5e7dfd8c.ngrok-free.app'],
+    domains: ['f8af5e7dfd8c.ngrok-free.app'],
   },
 };
 
